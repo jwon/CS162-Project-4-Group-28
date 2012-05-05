5a27cc3e3a0f5cd8786c024783890765cbd3c66c
Jacob Rashoff <jacobrashoff@berkeley.edu>
2012-05-05 16:35:50 -0700

    KVClientHandler for part 1 is done

--- src/edu/berkeley/cs162/KVClientHandler.java
+++ src/edu/berkeley/cs162/KVClientHandler.java
@@ -30,9 +30,11 @@
  */
 package edu.berkeley.cs162;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 /**
  * This NetworkHandler will asynchronously handle the socket connections. 
@@ -79,6 +81,171 @@ public class KVClientHandler<K extends Serializable, V extends Serializable> imp
 	 */
 	@Override
 	public void handle(Socket client) throws IOException {
-		// implement me
+		//System.out.println("handle called");
+		ConnectionHandler newTask = new ConnectionHandler(client);
+		//System.out.println("Time to add to ThreadPool");
+		if(newTask.failed == false){
+			try {
+				threadpool.addToQueue(newTask);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}		
+		} else {
+			//System.out.println("xml parsing error");
+		}
+	
 	}
-}
+	
+	private class ConnectionHandler implements Runnable{
+		boolean failed = false;
+		Socket s1;
+		KVMessage message;
+		
+		public ConnectionHandler(Socket client) throws SocketTimeoutException, IOException {
+			//System.out.println("ConnectionHandler constructor called");
+			this.s1 = client;
+			KVMessage response = new KVMessage("resp", null, null);
+			String xml = null;
+			
+			try {
+				//System.out.println("Getting inputstream");
+				message = new KVMessage(s1.getInputStream());
+				//System.out.println("Got inputstream and parsed message");
+			} catch (KVException e) {
+				//System.out.println("RAWR, THIS IS NOT GOOD");
+				FilterOutputStream fos = new FilterOutputStream(s1.getOutputStream());
+				fos.flush();
+				//System.out.println("KVException caught line 94");
+				response.setMessage(e.getMsg().getMessage());
+				try {
+					xml = response.toXML();
+				} catch (KVException e1) {
+					//System.out.println(e1.getMsg().getMessage());
+					xml = "xml parsing error line 104";
+				}
+				
+				byte[] xmlBytes = xml.getBytes();
+				try{
+						fos.write(xmlBytes);
+						fos.flush();
+					} catch (IOException e2){
+						//System.out.println("IO Error line 111");
+					}
+				s1.close();
+				failed = true;
+			}
+			//System.out.println("KVMessage from client:");
+			//System.out.println(message);
+		}//End of Constructor
+		
+		public void run() {
+			FilterOutputStream fos = null;
+			try {
+				fos = new FilterOutputStream(s1.getOutputStream());
+				fos.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+			KVMessage response = null;
+			String xml = "xml parsing error line 129";
+			if(message.getMsgType().equals("getreq")) {
+				try {
+					V value = (V)tpcMaster.handleGet(message);
+					//System.out.println("Value from GET: " + value);
+					response = new KVMessage("resp" , message.getKey(), value, null, "Success");
+				} catch (KVException e) {
+					response = new KVMessage("resp", null, 
+							null, null, e.getMsg().getMessage());		
+				} finally {
+					try {
+						xml = response.toXML();
+						//System.out.println("XML RESPONSE: " + xml);
+					} catch (KVException e1) {
+						//System.out.println("Fail XML conversion");
+					}
+					byte[] xmlBytes = xml.getBytes();
+					try{
+						fos.write(xmlBytes);
+						fos.flush();
+					} catch (IOException e){
+						//System.out.println("IO Error");
+					}
+					try {
+						s1.shutdownOutput();
+					} catch (IOException e) {
+						
+					}
+				} 
+				//System.out.println("******************************");
+				
+			} else if (message.getMsgType().equals("putreq")) {
+				 try {
+					boolean result = tpcMaster.performTPCOperation(message, true);
+					String resultString; if (result) resultString = "True"; else resultString = "False";
+					response = new KVMessage("resp" , null, null, resultString, "Success");
+
+				} catch (KVException e) {
+					response = new KVMessage("resp", null, 
+							null, null, e.getMsg().getMessage());
+				} finally {
+					try {
+						xml = response.toXML();
+						//System.out.println("XML RESPONSE: " + xml);
+					} catch (KVException e1) {
+						//System.out.println("Fail XML conversion");
+					}
+					byte[] xmlBytes = xml.getBytes();
+					//System.out.println("Beginning response send");
+					try{
+						fos.write(xmlBytes);
+						fos.flush();
+					} catch (IOException e){
+						//System.out.println("IO Error");
+					}
+					try {
+						s1.shutdownOutput();
+					} catch (IOException e) {
+						
+					}
+				}
+				 //System.out.println("response sent");
+				 //System.out.println("******************************");
+				 
+			} else if (message.getMsgType().equals("delreq")) {
+				try {
+					tpcMaster.performTPCOperation(message, false);
+					response = new KVMessage("resp" , null, null, null, "Success");
+				} catch (KVException e) {
+					response = new KVMessage("resp", null, 
+							null, null, e.getMsg().getMessage());
+				} finally {
+					try {
+						xml = response.toXML();
+					} catch (KVException e1) {
+						//System.out.println("Fail XML conversion");
+					}
+					byte[] xmlBytes = xml.getBytes();
+					try{
+						fos.write(xmlBytes);
+						fos.flush();
+					} catch (IOException e){
+						//System.out.println("IO Error");
+					}
+					try {
+						s1.shutdownOutput();
+					} catch (IOException e) {
+						
+					}
+				}
+			}
+			
+			try {
+				s1.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}//End of Run
+	}//End of ConnectionHandler
+}//End of KVClientHandler
